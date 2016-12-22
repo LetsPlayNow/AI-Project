e897a1f9c070ca44d26f7e5981dbd1c35b8ce60e
Aleksey Titov <staffofmousecorp@gmail.com>
2016-12-22 22:08:18 +0300

    Write GameSession model tests and delete old test
    Refactor GameSession model
    Delete Game Integration test

--- app/models/game_session.rb
+++ app/models/game_session.rb
@@ -11,7 +11,6 @@ class GameSession < ActiveRecord::Base
     2
   end
 
-  # ����� ������� � �������� ������ (����� ����)
   def self.other_players_count
     GameSession.players_count - 1
   end
@@ -22,25 +21,29 @@ class GameSession < ActiveRecord::Base
 
   # rewrite this
   def has_players?
-    # players(true).all { |player| player.is_in_game}
-    has_players = false
-    players(true).each do |player|
-      has_players ||= player.is_in_game
-    end
-    has_players
+    players(true).any? { |player| player.is_in_game }
   end
 
   def winner
-    winner_id && User.find(winner_id)
+    if winner_id.present?
+      User.find(winner_id)
+    else
+      nil
+    end
   end
 
   def is_active?
     @is_active = time_remaining > 0
   end
 
+  # Returns remaining time in seconds
   def time_remaining
-    to_end = (end_time - Time.now).round
-    (to_end < 0) ? 0 : to_end
+    time_lasts = end_time - Time.now
+    if time_lasts < 0
+      time_lasts = 0
+    end
+
+    return time_lasts
   end
 
   private
--- test/models/game_session_test.rb
+++ test/models/game_session_test.rb
@@ -4,4 +4,30 @@ class GameSessionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "is active" do
+    game = GameSession.create
+
+    assert_equal(true, game.is_active?, "Created game is active")
+
+    game.created_at -= GameSession.game_duration
+    game.save
+
+    assert_equal(false, game.is_active?, "Created game is active")
+  end
+
+  test "has_players" do
+    game = GameSession.create
+    user = User.first
+
+    player = Player.create(game_session_id: game.id, user_id: user.id)
+    assert_equal(true, game.has_players?, "Should have active players")
+
+    player.is_in_game = false
+    player.save
+    assert_equal(false, game.has_players?, "Should have active players")
+
+    game.players.delete(player)
+    assert_equal(false, game.has_players?, "Should have active players")
+  end
 end
