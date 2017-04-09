454a68957a1d293a703fc4d31535d14d282f286b
Aleksey Titov <staffofmousecorp@gmail.com>
2017-04-09 18:09:11 +0300

    Redirrect devise secret key

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,4 +1,7 @@
 class ErrorsController < ApplicationController
+  # http://blog.grepruby.com/2015/04/custom-error-pages-with-rails-4.html
+  # https://wearestac.com/blog/dynamic-error-pages-in-rails
+
   def not_found
     render :status => 404
   end
